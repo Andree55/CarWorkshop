00aff4c83d718054b22a5476c115ed18f371d35c
Adnre55 <adrianwie55@gmail.com>
2024-08-13 11:02:09 +0200

    Mapping Dto

--- CarWorkShop.Application/Services/CarWorkshopService.cs
+++ CarWorkShop.Application/Services/CarWorkshopService.cs
@@ -1,4 +1,6 @@
-﻿using CarWorkshop.Domain.Interfaces;
+﻿using AutoMapper;
+using CarWorkshop.Application.CarWorkshop;
+using CarWorkshop.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +9,20 @@ using System.Threading.Tasks;
 
 namespace CarWorkshop.Application.Services
 {
-    internal class CarWorkshopService : ICarWorkshopService
+    public class CarWorkshopService : ICarWorkshopService
     {
         private readonly ICarWorkshopRepository _carWorkshopRepository;
+        private readonly IMapper _mapper;
 
-        public CarWorkshopService(ICarWorkshopRepository carWorkshopRepository)
+        public CarWorkshopService(ICarWorkshopRepository carWorkshopRepository,IMapper mapper)
         {
             _carWorkshopRepository = carWorkshopRepository;
+            _mapper = mapper;
         }
-        public async Task Create(Domain.Entities.CarWorkshop carWorkshop)
+        public async Task Create(CarWorkshopDto carWorkshopDto)
         {
+            var carWorkshop=_mapper.Map<Domain.Entities.CarWorkshop>(carWorkshopDto);
+
             carWorkshop.EncodeName();
             await _carWorkshopRepository.Create(carWorkshop);
         }
--- CarWorkshop.MVC/Controllers/CarWorkshopController.cs
+++ CarWorkshop.MVC/Controllers/CarWorkshopController.cs
@@ -1,6 +1,7 @@
 ﻿using CarWorkshop.Domain.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using CarWorkshop.Application.Services;
+using CarWorkshop.Application.CarWorkshop;
 
 namespace CarWorkshop.MVC.Controllers
 {
@@ -19,7 +20,7 @@ namespace CarWorkshop.MVC.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Domain.Entities.CarWorkshop carWorkshop)
+        public async Task<IActionResult> Create(CarWorkshopDto carWorkshop)
         {
             await _carWorkshopService.Create(carWorkshop);
             return RedirectToAction(nameof(Create)); // TODO: Refactor
