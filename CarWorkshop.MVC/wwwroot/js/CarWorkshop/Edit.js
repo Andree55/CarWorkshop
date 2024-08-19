e7f6701242777bd4e0c2b508f20da61988f914b0
Andree55 <adrianwie55@gmail.com>
2024-08-20 00:48:19 +0200

    Reusing Components Added

--- CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
+++ CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
@@ -1,39 +1,7 @@
 ﻿$(document).ready(function () {
 
-    const RenderCarworkshopServices = (services, container) => {
-        container.empty();
+    LoadCarWorkshopServices()
 
-        for (const service of services) {
-            container.append(
-                `<div class="card border-secondary mb-3" style="max-width: 18rem;">
-          <div class="card-header">${service.cost}</div>
-          <div class="card-body">
-            <h5 class="card-title">${service.description}</h5> 
-          </div>
-        </div>`)
-        }
-    }
-
-    const LoadCarWQorkshopServices = () => {
-        const container = $("#services")
-        const carWorkshopEncodedName = container.data("encodedName");
-
-        $.ajax({
-            url: '/CarWorkshop/${carWorkshopEncodedName}/CarWorkshopService',
-            type: 'get',
-            success: function (data) {
-                if (!data.Length) {
-                    container.html("There are no services for this car workshop")
-                } else {
-                    RenderCarworkshopServices(data,container)
-                }
-            },
-            error: function () {
-                toastr["error"]("Something went wrong")
-            }
-        })
-    }
-    LoadCarWQorkshopServices();
 
     $("#createCarWorkshopServiceModal form").submit(function (event) {
         event.preventDefault();
@@ -44,6 +12,7 @@
             data: $(this).serialize(),
             success: function (data) {
                 toastr["success"]("Created carworkshop service")
+                LoadCarWorkshopServices()
             },
             error: function () {
                 toastr["error"]("Something went wrong")
