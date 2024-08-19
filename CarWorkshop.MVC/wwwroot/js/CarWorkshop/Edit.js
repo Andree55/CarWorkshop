4ba239c28a20c7744075ee7c665c0a1bd3cd773d
Andree55 <adrianwie55@gmail.com>
2024-08-20 00:34:01 +0200

    Async Rendering Data Added

--- CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
+++ CarWorkshop.MVC/wwwroot/js/CarWorkshop/Edit.js
@@ -1,4 +1,40 @@
 ﻿$(document).ready(function () {
+
+    const RenderCarworkshopServices = (services, container) => {
+        container.empty();
+
+        for (const service of services) {
+            container.append(
+                `<div class="card border-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">${service.cost}</div>
+          <div class="card-body">
+            <h5 class="card-title">${service.description}</h5> 
+          </div>
+        </div>`)
+        }
+    }
+
+    const LoadCarWQorkshopServices = () => {
+        const container = $("#services")
+        const carWorkshopEncodedName = container.data("encodedName");
+
+        $.ajax({
+            url: '/CarWorkshop/${carWorkshopEncodedName}/CarWorkshopService',
+            type: 'get',
+            success: function (data) {
+                if (!data.Length) {
+                    container.html("There are no services for this car workshop")
+                } else {
+                    RenderCarworkshopServices(data,container)
+                }
+            },
+            error: function () {
+                toastr["error"]("Something went wrong")
+            }
+        })
+    }
+    LoadCarWQorkshopServices();
+
     $("#createCarWorkshopServiceModal form").submit(function (event) {
         event.preventDefault();
 
